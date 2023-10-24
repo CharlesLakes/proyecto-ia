f18e10828700837b1a4fb22f401a618d0b813895
Carlos Cristóbal Lagos Cortés <46362356+CharlesLakes@users.noreply.github.com>
2023-10-24 20:54:50 -0300

    Update defs.hpp

--- defs.hpp
+++ defs.hpp
@@ -4,7 +4,7 @@
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
 #define defs_hpp
-#define forn(i,i0,n) for(ll i = i0; i < n; i++)
+#define forn(i,i0,n) for(ll i = i0; i < (ll) n; i++)
 #define ll int64_t
 #define double long double
 #define fastio ios_base::sync_with_stdio(false),cin.tie(NULL);
